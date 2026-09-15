ef47e8b8e2bcfcb6d185577029994cbe1f5cf64d
Daniel-Chen-The-Creator <danielchen51666@gmail.com>
2026-09-15 11:05:45 -0400

    more logical code

--- 4.Merge_sort/1.Standard_algo.py
+++ 4.Merge_sort/1.Standard_algo.py
@@ -1,38 +1,35 @@
-def merge(A, p, q, r):
-    #A = [8,3]
-    L = A[p:q+1] #[8]         
-    R = A[q+1:r+1] #[3]      
-
-    i = j = 0  #i is the position for L & j is the position for R        
-    k = p               
-
-    while i < len(L) and j < len(R):   
-        if L[i] <= R[j]:              
-            A[k] = L[i]                
-            i += 1                     
-        else:                           
-            A[k] = R[j]               
-            j += 1                     
-        k += 1                         
-
-    while i < len(L):                  
-        A[k] = L[i]                    
-        i += 1                         
-        k += 1                         
-
-    while j < len(R):                 
-        A[k] = R[j]                    
-        j += 1                         
-        k += 1                         
-
-
-def merge_sort(A, p, r):
-   
-    if p < r:                          
-        q = (p + r) // 2               
-        merge_sort(A, p, q)           
-        merge_sort(A, q + 1, r)        
-        merge(A, p, q, r)              
+def merge(arr,p,q,r):
+    arr_L = arr[p:q+1]
+    arr_R = arr[q+1:r+1]
+    
+    L = R = 0
+    k = p 
+    
+    while L < len(arr_L) and R < len(arr_R):
+        if arr_L[L] <= arr_R[R]:
+            arr[k] = arr_L[L]
+            L += 1 
+        else:
+            arr[k] = arr_R[R]
+            R += 1
+        k+=1 
+    
+    while L < len(arr_L):
+        arr[k] = arr_L[L]
+        L += 1
+        k += 1 
+        
+    while R < len(arr_R):
+        arr[k] = arr_R[R]
+        R += 1 
+        k += 1 
+    
+def merge_sort(arr,p,r):
+    if p<r:
+        q = (p+r)//2
+        merge_sort(arr,p,q)
+        merge_sort(arr,q+1,r)
+        merge(arr,p,q,r)
 
 a = [8, 3, 5, 4, 7, 6, 1, 2]
 merge_sort(a, 0, len(a) - 1)
