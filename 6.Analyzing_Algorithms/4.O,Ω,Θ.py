c03b2efda57ec4961b1049b949f0883340de5ef7
Daniel-Chen-The-Creator <danielchen51666@gmail.com>
2026-09-14 22:36:04 -0400

    notes

---
+++ 6.Analyzing_Algorithms/4.O,Ω,Θ.py
@@ -0,0 +1,11 @@
+#1.O(g(n))
+#   Upper bound
+#   Meaning: Grows at most at this rate
+
+#2.Ω(g(n))
+#   Lower bound
+#   Meaning: Grows at least at this rate
+
+#3.Θ(g(n))	
+#   Tight bound	 
+#   Meaning: Grows exactly at this asymptotic rate
