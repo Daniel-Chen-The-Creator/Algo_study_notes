be69e21fb2ad16f06556022cfab92b032d6a6ee8
Daniel-Chen-The-Creator <danielchen51666@gmail.com>
2026-09-18 17:08:45 -0400

    111

---
+++ 3.Insertion_sort/Medium: Sort in Descending Order.py
@@ -0,0 +1,9 @@
+nums = [4, 9, 4, 1, 7, 3]
+for i in range(len(nums)):
+    key = nums[i]
+    j = i - 1 
+    while j >= 0 and nums[j] < key:
+        nums[j+1] = nums[j]
+        j -= 1
+    nums[j + 1] = key
+print(nums)
