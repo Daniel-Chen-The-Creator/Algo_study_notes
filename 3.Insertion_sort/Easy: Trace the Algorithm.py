0014134749ea27a1bfc3b307e20fbd1e45a051e2
Daniel-Chen-The-Creator <danielchen51666@gmail.com>
2026-09-18 16:56:49 -0400

    practice

---
+++ 3.Insertion_sort/Easy: Trace the Algorithm.py
@@ -0,0 +1,10 @@
+nums = [7, 3, 5, 2, 6]
+for i in range(len(nums)):
+    key = nums[i]
+    j = i - 1
+    while j >= 0 and nums[j] > key:
+        nums[j + 1] = nums[j]
+        j -= 1
+    nums[j + 1] = key
+
+print(nums)
